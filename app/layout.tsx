2e33efffe4703d3477360421b2822dbf849188ab
wmoooid <threeqletters@yandex.ru>
2024-01-26 16:57:19 +0400

    Next Font Module

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,32 @@ import Footer from '@/components/footer';
 import Stripes from '@/components/stripes';
 import '@/node_modules/normalize.css';
 import '@/styles/globals.scss';
+import localFont from 'next/font/local';
+
+const Muller = localFont({
+    src: [
+        {
+            path: './fonts/MullerLight.woff2',
+            weight: '300',
+            style: 'normal',
+        },
+        {
+            path: './fonts/MullerRegular.woff2',
+            weight: '400',
+            style: 'normal',
+        },
+        {
+            path: './fonts/MullerMedium.woff2',
+            weight: '500',
+            style: 'normal',
+        },
+        {
+            path: './fonts/MullerBold.woff2',
+            weight: '600',
+            style: 'normal',
+        },
+    ],
+});
 
 export const metadata = {
     title: 'Living.io',
@@ -11,7 +37,7 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang='ru'>
+        <html lang='ru' className={Muller.className}>
             <body>
                 <Header />
                 <main>{children}</main>

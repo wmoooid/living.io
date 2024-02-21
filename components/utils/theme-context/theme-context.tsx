dc08ce1fa8c00faaac5cc38df03a5a72e98f7956
wmoooid <threeqletters@yandex.ru>
2024-02-21 20:19:52 +0400

    theme fixes

--- components/utils/theme-context/theme-context.tsx
+++ components/utils/theme-context/theme-context.tsx
@@ -13,19 +13,29 @@ type ThemeContext = {
     setCurrentTheme: React.Dispatch<React.SetStateAction<Theme>>;
 };
 
-const getPrefferedColorScheme = () => (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+const getPreferredColorScheme = () => {
+    if (window.matchMedia) {
+        return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    }
+
+    return 'light';
+};
 
 export const ThemeContext = createContext({} as ThemeContext);
 
 export default function ThemeContextProvider({ children }: ThemeContextProviderProps) {
-    const [currentTheme, setCurrentTheme] = useState<Theme>(getPrefferedColorScheme());
+    const [currentTheme, setCurrentTheme] = useState<Theme>(getPreferredColorScheme);
+
+    useEffect(() => {
+        const handleThemeChange = () => setCurrentTheme(getPreferredColorScheme());
+        window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', handleThemeChange);
+        return () => window.matchMedia('(prefers-color-scheme: dark)').removeEventListener('change', handleThemeChange);
+    }, []);
 
     useEffect(() => {
         const docClassList = document.documentElement.classList;
-
         docClassList.remove('light', 'dark');
-        if (currentTheme === 'light') docClassList.add('light');
-        if (currentTheme === 'dark') docClassList.add('dark');
+        docClassList.add(currentTheme);
     }, [currentTheme]);
 
     return <ThemeContext.Provider value={{ currentTheme, setCurrentTheme }}>{children}</ThemeContext.Provider>;
